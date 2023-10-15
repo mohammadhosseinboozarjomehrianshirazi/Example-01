3f65bcc16fea6aaccb256ceaee4be54e0e6233d9
mohammadhosseinboozarjomehrianshirazi <130217722+mohammadhosseinboozarjomehrianshirazi@users.noreply.github.com>
2023-10-15 20:40:49 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int next = 0;
+            int prev = 0;
+            for (int i = 0; i <= 10; i++) 
+            {
+                Console.WriteLine(next);
+                if(next==0)
+                {
+                    next = 1;
+                }
+                else
+                {
+                    int temp = next;
+                    next = next + prev;
+                    prev = next;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
